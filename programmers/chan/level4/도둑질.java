7f66006981f1da5001155653a258fdd323b75031
BakChanGyu <103313634+BakChanGyu@users.noreply.github.com>
2024-01-17 17:18:45 +0900

    lv4 도둑질(최종)

--- programmers/chan/level4/도둑질.java
+++ programmers/chan/level4/도둑질.java
@@ -2,26 +2,18 @@ class Solution {
     public int solution(int[] money) {
         int answer = 0;
         int n = money.length;
-        
-        for (int start = 0; start < 3; start++) {
-            boolean[] isVisited = new boolean[n];
+
+        for (int start = 0; start <= 1; start++) {
             int[] sum = new int[n];        
-            int preSum = 0;
             
             for (int i = start; i < n; i++) {
-                if (isVisited[i] == false) {
-                    isVisited[i] = true;
-                    sum[i] = money[i] + preSum;
-
-                    int pre = (i - 1 + n) % n;
-                    int next = (i + 1 + n) % n;
-
-                    isVisited[pre] = true;
-                    isVisited[next] = true;
-
-                    preSum = sum[i];
-                    answer = Math.max(answer, sum[i]);
-                }
+                sum[i] = Math.max(money[i] + sum[(i - 2 + n) % n], sum[(i - 1 + n) % n]);
+            }
+            
+            if (start == 0) {
+                answer = sum[n - 2];
+            } else {
+                answer = Math.max(answer, sum[n - 1]);
             }
         }
         
